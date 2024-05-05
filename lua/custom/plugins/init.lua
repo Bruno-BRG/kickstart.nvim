e8e6ff9bb81a6ab0f66ae3d55863bacaa7bc6c87
Bruno Guimarães <88166479+Frykits@users.noreply.github.com>
2024-05-05 10:35:29 -0300

    Add files via upload

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,54 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+--break long line at blank spaces
+vim.opt.linebreak = true
+
+-- wrap long ling into the next line
+vim.opt.wrap = true
+
+--indentation size
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+return {
+  --snippets plugin
+  {
+    'L3MON4D3/LuaSnip',
+    -- follow latest release.
+    version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = 'make install_jsregexp',
+
+    dependencies = { 'rafamadriz/friendly-snippets' },
+
+    config = function()
+      local ls = require 'luasnip'
+      ls.filetype_extend('javascript', { 'jsdoc' })
+
+      --- TODO: What is expand?
+      vim.keymap.set({ 'i' }, '<C-s>e', function()
+        ls.expand()
+      end, { silent = true })
+
+      vim.keymap.set({ 'i', 's' }, '<C-s>;', function()
+        ls.jump(1)
+      end, { silent = true })
+      vim.keymap.set({ 'i', 's' }, '<C-s>,', function()
+        ls.jump(-1)
+      end, { silent = true })
+
+      vim.keymap.set({ 'i', 's' }, '<C-E>', function()
+        if ls.choice_active() then
+          ls.change_choice(1)
+        end
+      end, { silent = true })
+    end,
+  },
+
+  --Github Copilot
+  {
+    'github/copilot.vim',
+  },
+}
